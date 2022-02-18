5e955aff24ea9ba279ebdb5024412eba5b5000ca
Victor Gasior <vicagasi@ut.utm.edu>
2022-02-17 19:54:11 -0600

    Add project files.

---
+++ CW7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 200;
+            int height = 200;
+            TextField tx = new TextField(width, height);
+            tx.draw();
+
+            BorderDecorator bd = new BorderDecorator(tx);
+            ScrollDecorator sd = new ScrollDecorator(tx);
+        }
+    }
+}
---
+++ CW7/Widget.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW7
+{
+    public abstract class Widget
+    {
+        virtual public void draw()
+        {
+            Console.WriteLine("Something is drawn");        
+        }
+    }
+
+    public class TextField : Widget
+    {
+        int width = 100;
+        int height = 100;
+        public TextField(int width, int height)
+        {
+            Console.WriteLine("A text field of width " + width + " and height " + height + " has been made.");
+        }
+
+        public override void draw()
+        {
+            base.draw();
+        }
+    }
+
+    public abstract class Decorator : Widget
+    {
+        public Widget wid;
+
+        public Decorator(Widget wid)
+        {
+            Console.WriteLine("The box is decorated");
+        }
+
+        public override void draw()
+        {
+            base.draw();
+        }
+    }
+
+    public class BorderDecorator : Decorator
+    {
+        public BorderDecorator(Widget wid) : base(wid)
+        {
+            Console.WriteLine("The box's border is decorated");
+        }
+
+        public override void draw()
+        {
+            base.draw();
+        }
+    }
+
+    public class ScrollDecorator : Decorator
+    {
+        public ScrollDecorator(Widget wid) : base(wid)
+        {
+            Console.WriteLine("The box's scroll bar is decorated");
+        }
+
+        public override void draw()
+        {
+            base.draw();
+        }
+    }
+}
